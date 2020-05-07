0d5f9f3aa406f89314520caca76df51c5d298297
Mónica Polanco <mpolan16@g.holycross.edu>
2020-05-07 18:05:15 -0400

    put prompts into an array

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+
 const questions = [
-      {
-          type: 'input',
-          name: 'username',
-          message: 'What is your GitHub username? ',
-      },
-      {
-          type: 'input',
-          name: 'project name',
-          message: 'What is your project name? ' ,
-      },
-      {
-          type: 'input',
-          name: 'description',
-          message: 'Please write a short description of your project:',
-      },
-      {
-          type: 'input',
-          name: 'license',
-          message: 'What kind of license should your project have? ',
-      },
-      {
-          type: 'input',
-          name: 'command',
-          message: 'What command should be run to install dependencies? ',
-      },
-      {
-          type: 'input',
-          name: 'repo info',
-          message: 'What does the user need to know about using the repo? ',
-      },
-      {
-          type: 'input',
-          name: 'contributing',
-          message: 'What does the user need to know about contributing to the repo? ',
-      },
-  ];
-function askQuestion() {
-    inquirer.prompt(questions)
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username? ',
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is your project name? ' ,
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What kind of license should your project have? ',
+    },
+    {
+        type: 'input',
+        name: 'command',
+        message: 'What command should be run to install dependencies? ',
+    },
+    {
+        type: 'input',
+        name: 'repoInfo',
+        message: 'What does the user need to know about using the repo? ',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo? ',
+    },
+];
 
-} 
-function writeToFile(fileName, data) {
-}
-
-
-function init() {
-askQuestion();
+const ask = async () => {
+  return await inquirer.prompt(questions);
 };
 
+const init = async () => {
+    const responses = await ask();
+    console.log(responses);
+};
 init();
